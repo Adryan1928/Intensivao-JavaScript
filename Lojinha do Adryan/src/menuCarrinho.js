e500ec60202bcec2a80ce8eac1e76b874f0bf9c7
Adryan1928 <adryanoliveira1928@gmail.com>
2023-08-30 21:05:17 -0300

    Aula 003

--- Lojinha do Adryan/src/menuCarrinho.js
+++ Lojinha do Adryan/src/menuCarrinho.js
@@ -1,3 +1,5 @@
+import { catalogo } from "./utilidades"
+
 var carrinho = document.getElementById('carrinho')
 function abrirCarrinho() {
     carrinho.classList.add('right-[0px]')
@@ -15,4 +17,25 @@ export function inicializarCarrinho() {
 
     botaoFecharCarrinho.addEventListener('click', fecharCarrinho)
     botaoAbrirCarrinho.addEventListener('click', abrirCarrinho)
+}
+
+export function adicionarAoCarrinho (idProduto) {
+    const produto = catalogo.find((p) => p.id === idProduto)
+    const containerProdutosCarrinho = document.getElementById('produtos-carrinho')
+    const cartaoProdutoCarrinho = `
+    <article class="flex bg-slate-100 border rounded-lg p-1 relative">
+        <button id="fechar-carrinho" class="absolute top-0 right-2">
+            <i class="fa-solid fa-circle-xmark text-slate-500 hover:text-slate-800"></i>
+        </button>
+        
+        <img src="./assets/img/${produto.imagem}" alt="Produto ${produto.id}" class="h-24 rounded-lg">
+
+        <div class="py-2">
+            <p class="text-slate-900 text-sm" >${produto.nome}</p>
+            <p class="text-slate-400 text-xs">Tamanho: M</p>
+            <p class="text-green-700 text-lg">$${produto.preco}</p>
+        </div>
+    </article>
+    `
+    containerProdutosCarrinho.innerHTML += cartaoProdutoCarrinho
 }
